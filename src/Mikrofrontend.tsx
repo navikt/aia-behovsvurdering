d644b2d7fa863232a1399fef55363aec86b84877
Aurora Christine Hofman <aurora.christine.hofman@nav.no>
2024-05-16 12:35:44 +0200

    slett opplysnigner om arbeidssøker context

--- src/Mikrofrontend.tsx
+++ src/Mikrofrontend.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 
 import * as SprakValg from './contexts/sprak';
 import { ArbeidssokerperioderProvider } from './contexts/arbeidssokerperioder';
-import { OpplysningerOmArbeidssokerProvider } from './contexts/opplysninger-om-arbeidssoker';
 import { ProfileringProvider } from './contexts/profilering';
 import { BehovsvurderingProvider } from './contexts/behovsvurdering';
 import { MoetestoetteProvider } from './contexts/moetestoette';
@@ -21,17 +20,15 @@ function Mikrofrontend() {
     return (
         <SprakValg.SprakContext.Provider value={valgtSprak}>
             <ArbeidssokerperioderProvider>
-                <OpplysningerOmArbeidssokerProvider>
-                    <ProfileringProvider>
-                        <BehovsvurderingProvider>
-                            <MoetestoetteProvider>
-                                <VedtaksstoetteProvider>
-                                    <AiaWrapper />
-                                </VedtaksstoetteProvider>
-                            </MoetestoetteProvider>
-                        </BehovsvurderingProvider>
-                    </ProfileringProvider>
-                </OpplysningerOmArbeidssokerProvider>
+                <ProfileringProvider>
+                    <BehovsvurderingProvider>
+                        <MoetestoetteProvider>
+                            <VedtaksstoetteProvider>
+                                <AiaWrapper />
+                            </VedtaksstoetteProvider>
+                        </MoetestoetteProvider>
+                    </BehovsvurderingProvider>
+                </ProfileringProvider>
             </ArbeidssokerperioderProvider>
         </SprakValg.SprakContext.Provider>
     );
