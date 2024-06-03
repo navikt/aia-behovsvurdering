a3352e6f7eb6365bdd84d38d303af3f19e42442e
Jostein Holje <jostein.holje@nav.no>
2024-06-03 14:59:58 +0200

    Oppdater behovsvurdering-state ved POST i story

--- src/aia.stories.tsx
+++ src/aia.stories.tsx
@@ -1,18 +1,44 @@
 import { Meta, StoryObj } from '@storybook/react';
-import Behovsvurdering from './aia';
+import Behovsvurdering, { BehovsvurderingProps } from './aia';
 import arbeidssokerperioderMock from './mocks/arbeidssokerperioder-mock';
 import profileringMock from './mocks/profilering-mock';
-import behovsvurderingMock from './mocks/behovsvurdering-mock';
 import moetestoetteMock from './mocks/moetestoette-mock';
+import { http, HttpResponse } from 'msw';
+import { BEHOV_FOR_VEILEDNING_URL, OPPRETT_DIALOG_URL } from './urls/api';
+import { BehovsvurderingProvider, useBehovsvurdering } from './contexts/behovsvurdering';
+
+function BehovsvurderingsWrapper(props: BehovsvurderingProps) {
+    const { behovForVeiledning } = useBehovsvurdering();
+    return <Behovsvurdering {...props} behovsvurdering={behovForVeiledning} />;
+}
 
 const meta = {
     title: 'Behovsvurdering',
-    component: Behovsvurdering,
-    decorators: [],
+    component: BehovsvurderingsWrapper,
+    decorators: [
+        (Story, ctx) => (
+            <BehovsvurderingProvider>
+                <Story />
+            </BehovsvurderingProvider>
+        ),
+    ],
     tags: ['autodocs'],
     parameters: {
         backgrounds: {
             default: 'dark',
+        },
+        msw: {
+            handlers: [
+                http.post(OPPRETT_DIALOG_URL, () => HttpResponse.json({ id: '1234' })),
+                http.post(BEHOV_FOR_VEILEDNING_URL, async ({ request }) => {
+                    const data = (await request.json()) as any;
+                    return HttpResponse.json({
+                        dato: new Date().toISOString().substring(0, 10),
+                        oppfolging: data.oppfolging,
+                    });
+                }),
+                http.get(BEHOV_FOR_VEILEDNING_URL, () => new HttpResponse(null, { status: 204 })),
+            ],
         },
     },
     args: {},
@@ -27,16 +53,6 @@ export const Arbeidssøker: Story = {
         profilering: profileringMock as any,
         arbeidssokerperioder: arbeidssokerperioderMock as any,
         behovsvurdering: null,
-        moetestoette: moetestoetteMock as any,
-    },
-};
-
-export const ErPermittert: Story = {
-    args: {
-        sprak: 'nb',
-        profilering: profileringMock as any,
-        arbeidssokerperioder: arbeidssokerperioderMock as any,
-        behovsvurdering: behovsvurderingMock as any,
         moetestoette: moetestoetteMock as any,
     },
 };
