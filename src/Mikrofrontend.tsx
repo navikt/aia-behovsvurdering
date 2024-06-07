33e534a5c6cee4a58e39cedf8bd5651a294e94f2
Jostein Holje <jostein.holje@nav.no>
2024-06-07 09:49:54 +0200

    Fjern feature-toggling fra <Mikrofrontend>

--- src/Mikrofrontend.tsx
+++ src/Mikrofrontend.tsx
@@ -9,37 +9,10 @@ import { MoetestoetteProvider } from './contexts/moetestoette';
 import AiaWrapper from './aia-wrapper';
 import './index.css';
 import { initAmplitude } from './lib/amplitude';
-import { FEATURE_URL } from './urls/api';
-import fetcher from './lib/http';
-export const FEATURE_TOGGLE = 'aia.bruk-opplysninger-om-arbeidssoker-api';
-
-const useFeatures = () => {
-    const [isLoading, setIsLoading] = useState<boolean>(true);
-    const [features, setFeatures] = useState<any>();
-
-    const fetchFeatures = async () => {
-        try {
-            setFeatures(await fetcher(`${FEATURE_URL}?feature=${FEATURE_TOGGLE}`));
-        } catch (err) {
-            setFeatures({ [FEATURE_TOGGLE]: false });
-        } finally {
-            setIsLoading(false);
-        }
-    };
-
-    useEffect(() => {
-        fetchFeatures();
-    }, []);
-
-    return {
-        isLoading,
-        features,
-    };
-};
 
 function Mikrofrontend() {
     const [valgtSprak, setValgtSprak] = useState<SprakValg.State>(SprakValg.initialState);
-    const { isLoading, features } = useFeatures();
+
     useEffect(() => {
         setValgtSprak(SprakValg.hentSprakValgFraUrl);
     }, [window.location.href]);
@@ -47,10 +20,6 @@ function Mikrofrontend() {
     useEffect(() => {
         initAmplitude();
     }, []);
-
-    if (isLoading || features[FEATURE_TOGGLE] === false) {
-        return null;
-    }
 
     return (
         <SprakValg.SprakContext.Provider value={valgtSprak}>
