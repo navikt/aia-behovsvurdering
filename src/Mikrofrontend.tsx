05b895527643c4261847e5672f2893a6a1a49797
Jostein Holje <jostein.holje@nav.no>
2024-06-10 11:31:59 +0200

    Legger til aia.nedetid feature toggle killswitch

--- src/Mikrofrontend.tsx
+++ src/Mikrofrontend.tsx
@@ -9,9 +9,38 @@ import { MoetestoetteProvider } from './contexts/moetestoette';
 import AiaWrapper from './aia-wrapper';
 import './index.css';
 import { initAmplitude } from './lib/amplitude';
+import fetcher from './lib/http';
+import { FEATURE_URL } from './urls/api';
+
+export const FEATURE_TOGGLE = 'aia.nedetid';
+
+const useFeatures = () => {
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [features, setFeatures] = useState<any>();
+
+    const fetchFeatures = async () => {
+        try {
+            setFeatures(await fetcher(`${FEATURE_URL}?feature=${FEATURE_TOGGLE}`));
+        } catch (err) {
+            setFeatures({ [FEATURE_TOGGLE]: true });
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchFeatures();
+    }, []);
+
+    return {
+        isLoading,
+        features,
+    };
+};
 
 function Mikrofrontend() {
     const [valgtSprak, setValgtSprak] = useState<SprakValg.State>(SprakValg.initialState);
+    const { isLoading, features } = useFeatures();
 
     useEffect(() => {
         setValgtSprak(SprakValg.hentSprakValgFraUrl);
@@ -20,6 +49,14 @@ function Mikrofrontend() {
     useEffect(() => {
         initAmplitude();
     }, []);
+
+    if (isLoading) {
+        return null;
+    }
+
+    if (features[FEATURE_TOGGLE] === true) {
+        return <div data-testid={'toggled-off'} style={{ display: 'none' }} />;
+    }
 
     return (
         <SprakValg.SprakContext.Provider value={valgtSprak}>
