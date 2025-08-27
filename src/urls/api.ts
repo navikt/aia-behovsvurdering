fd097d826efbddae14d604afb298f8e0befb4ec1
Geir Gåsodden <geir.gasodden@gmail.com>
2025-08-27 13:07:47 +0200

    c peker om kall mot oppslag til å bruke v2

--- src/urls/api.ts
+++ src/urls/api.ts
@@ -3,7 +3,7 @@ import { aiaBackendUrl } from './index';
 export const AIA_BACKEND = aiaBackendUrl,
     FEATURE_URL = `${AIA_BACKEND}/unleash`,
     BEHOV_FOR_VEILEDNING_URL = `${AIA_BACKEND}/behov-for-veiledning`,
-    ARBEIDSOKERPERIODER_URL = `${AIA_BACKEND}/arbeidssokerregisteret/v1/arbeidssoekerperioder`,
+    ARBEIDSOKERPERIODER_URL = `${AIA_BACKEND}/arbeidssokerregisteret-v2/v1/arbeidssoekerperioder`,
     BEHOVSVURDERING_URL = `${AIA_BACKEND}/behov-for-veiledning`,
     OPPRETT_DIALOG_URL = `${AIA_BACKEND}/dialog`,
-    PROFILERING_URL = `${AIA_BACKEND}/arbeidssokerregisteret/v1/profilering`;
+    PROFILERING_URL = `${AIA_BACKEND}/arbeidssokerregisteret-v2/v1/profilering`;
